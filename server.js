ae47b10527a2828008d1f427f9eb81dedd939026
matthewsublett-22 <matthewsublett@gmail.com>
2026-04-13 16:13:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const TOGETHER_API_KEY = 'tgp_v1_ogDvjjjDx6b3Bn2quAFLkOWMEmv63Z323b7tGgvxjIQ';
+const TOGETHER_API_KEY = 'tgp_v1_FYDRswdoKZnrqDlvufyObVI_X29cAFfBS53ny4p4OC4';
 const MODEL = 'meta-llama/Llama-3.3-70B-Instruct-Turbo';
 
 app.post('/api/chat', async (req, res) => {
